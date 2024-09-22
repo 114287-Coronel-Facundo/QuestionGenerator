60bdc83b88e42e269da6d0ca8dba3aa871b1ed79
77835_CORONEL <77835@industrial.frc.utn.edu.ar>
2024-09-22 15:41:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from langchain_community.llms import Ollama
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
